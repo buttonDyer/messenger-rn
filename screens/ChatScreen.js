d63526d1f1ae3f522800cc871688959cb8611337
Roman <supernaturalmlady@gmail.com>
2023-04-23 18:49:18 +0600

    custom header arrow back added

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
-const ChatScreen = () => {
+import { React, useLayoutEffect } from 'react'
+
+import { Avatar } from '@rneui/base'
+
+import { AntDesign } from '@expo/vector-icons'
+
+const ChatScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Chat',
+      headerTitleAlign: 'left',
+      headerTitle: () => (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            rounded
+            source={{
+              uri: 'https://www.mtsolar.us/wp-content/uploads/2020/04/avatar-placeholder.png',
+            }}
+          />
+          <Text
+            style={{
+              color: 'white',
+              marginLeft: 10,
+              fontWeight: 700,
+            }}
+          >
+            {route.params.chatName}
+          </Text>
+        </View>
+      ),
+      headerLeft: () => (
+        <TouchableOpacity
+          style={{
+            marginLeft: 10,
+          }}
+          onPress={navigation.goBack}
+        >
+          <AntDesign name="arrowleft" size={24} color="white" />
+        </TouchableOpacity>
+      ),
+      headerRight: () => (
+        <View>
+          <TouchableOpacity></TouchableOpacity>
+        </View>
+      ),
+    })
+  }, [navigation])
+
   return (
     <View>
-      <Text>ChatScreen</Text>
+      <Text>{route.params.chatName}</Text>
     </View>
   )
 }
